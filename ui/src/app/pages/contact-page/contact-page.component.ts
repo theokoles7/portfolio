9494bf398a1cc5bae778685c45d30c5029198869
theokoles <gabrieltrahan777@hotmail.com>
2023-06-02 14:25:20 -0500

    Initial Contact page

--- ui/src/app/pages/contact-page/contact-page.component.ts
+++ ui/src/app/pages/contact-page/contact-page.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-page.component.scss']
 })
 export class ContactPageComponent implements OnInit {
+  templates: {[id:string]:string} = {
+    'resume': 'Hello Gabriel,\n\tI work for [COMPANY]. We have an opening for a [POSITION], and I\'d like to request your résumé for review.',
+
+    'feedback': 'Hello Gabriel, here\'s my feedback:\n\nCritiques/Suggestions:\n------------------------\n\nCompliments:\n-------------\n',
+
+    'collab': 'Hello Gabriel,\n\tI\'m currently working on a project that I\'d like to collaborate with you on.\n\n\tProject description:\n\n\tIntended role: ',
+
+    'none': ''
+  }
+
+  message:string='';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  insertTemplate(template: string):void{
+    this.message = this.templates[template];    
   }
 
 }
